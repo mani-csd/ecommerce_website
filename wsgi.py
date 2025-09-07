b93493dbbe88e9b6a5dabae24628e299fd5a284b
Mani Chandra <bvmanichandra@gmail.com>
2025-09-07 22:55:17 +0530

    Prepare for Render deployment

--- wsgi.py
+++ wsgi.py
@@ -1,3 +1,6 @@
+from run import create_app
 from dotenv import load_dotenv
-load_dotenv()
-from run import app
+
+load_dotenv()  # load environment variables
+
+app = create_app()  # Gunicorn needs a variable named "app"
